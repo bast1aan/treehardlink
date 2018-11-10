3ca0ed13d7e2a22db9e4c8e37fa070112ac12d56
Bastiaan Welmers <bastiaan@welmers.net>
2018-11-10 12:19:36 +0100

    First excerpt, indexing file trees in sqlite
    Result is a SQLite file containing all files and its stat(2) properties.
    From there on the data can easily be analysed with SQL queries.

---
+++ treehardlink.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+import sqlite3
+import sys
+
+conn = sqlite3.connect('treehardlink.sqlite3')
+
+tbl = """
+CREATE TABLE files (
+inode INT PRIMARY KEY,
+dir VARCHAR,
+path VARCHAR,
+mode INT NULL,
+number_of_links INT NULL,
+uid INT NULL,
+gid INT NULL,
+size INT NULL,
+mtime INT NULL,
+ctime INT NULL
+);
+"""
+
+idxes = (
+    "CREATE INDEX idx_files_dir  ON files(dir)",
+    "CREATE INDEX idx_files_path  ON files(path)",
+    "CREATE INDEX idx_files_number_of_links ON files(number_of_links)",
+)
+
+c = conn.cursor()
+c.execute(tbl)
+conn.commit()
+
+
+for dir in sys.argv[1:]:
+    dir = dir.strip()
+    if not os.path.exists(dir):
+        print("Directory not found: {}".format(dir))
+        exit(1)
+    print("Getting file list of {}...".format(dir))
+    find_cmd = "find {} -type f".format(dir)
+    p = subprocess.Popen(find_cmd, shell=True, stdout=subprocess.PIPE)
+    filenames = []
+    for filename in p.stdout.readlines():
+        filenames.append(filename)
+    print("Statting files in dir {}...".format(dir))
+    for filename in filenames:
+        filename = filename.strip()
+        dirlen = len(dir)
+        path = filename[dirlen:]
+        s = os.stat(filename)
+        sql = "INSERT OR IGNORE INTO files (inode, dir, path, mode, number_of_links, uid, gid, size, mtime, ctime) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+        c.execute(sql, (s.st_ino, dir, path, s.st_mode, s.st_nlink, s.st_uid, s.st_gid, s.st_size, s.st_mtime, s.st_ctime))
+    conn.commit()
+
+print("Applying indexes...")
+for idx in idxes:
+    c.execute(idx)
+conn.commit()
+print("Done.")
+
+
